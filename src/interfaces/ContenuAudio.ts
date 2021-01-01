14381e320ed2204a55aea1263324d44463b9b260
gabrielseron <gabriel.seron.pro@gmail.com>
2021-01-01 18:55:08 +0100

    Add interfaces

---
+++ src/interfaces/ContenuAudio.ts
@@ -0,0 +1,11 @@
+export interface ContenuAudioInterfaces
+{
+    idAudio : number;
+    Nom : string;
+    url : string;
+    cover : string; // cover link
+    time : number; // duration in ms ?
+    type : string;
+    createdAt : Date;
+    UpdatedAt : Date;
+}
