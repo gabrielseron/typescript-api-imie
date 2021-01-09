b5fb1e96e9f5e659d2c562ad422a9a2d4ca5ee6e
Alpha <diengalphaoumar26@gmail.com>
2021-01-09 16:17:36 +0100

    commit de stripe

---
+++ server.ts
@@ -0,0 +1,42 @@
+var express = require('express');
+const stripe = require('stripe')('sk_test_51I6YU9KCUA5u3WGTKA3xuTolvhvPLz2j55ojR154HN0gafOj8FWARcqMbWuv9HFJGASNWIZ66oO2s7GVbKPy7gCE00GPPfUcFl');
+var app = express();
+
+app.listen(3000, function () {
+    console.log("server running");
+});
+
+
+var carte = function () {
+
+    var param = {
+    card :{
+        number: '4242424242424242',
+        exp_month: 2,
+        exp_year:2024,
+        cvc:'212'
+    }
+    }
+
+    stripe.tokens.create(param, function (err,token) {
+        if(err)
+        {
+            console.log("err: "+err);
+        }if(token)
+        {
+            console.log(token.card.exp_month);
+            console.log("success: "+JSON.stringify(token, null, 2));
+        }else{
+            console.log("Something wrong")
+        }
+    })
+}
+
+
+
+
+carte();
+
+app.post('/payment', function (req, res) {
+  res.send(""+carte);
+});
