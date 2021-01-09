9a0ffda71fc8df8f66878f059d3cc612b54eedb7
gabrielseron <47740013+gabrielseron@users.noreply.github.com>
2021-01-09 16:59:05 +0100

    Add files (merge doesn't work)

--- server.ts
+++ server.ts
@@ -1,3 +1,10 @@
+import bodyParser from "body-parser";
+import { config } from "dotenv";
+import express from "express";
+import cors from "cors";
+
+import { AuthentificationRoute } from "./src/routes/AuthentificationRoute";
+
 var express = require('express');
 const stripe = require('stripe')('sk_test_51I6YU9KCUA5u3WGTKA3xuTolvhvPLz2j55ojR154HN0gafOj8FWARcqMbWuv9HFJGASNWIZ66oO2s7GVbKPy7gCE00GPPfUcFl');
 var app = express();
@@ -40,3 +47,16 @@ carte();
 app.post('/payment', function (req, res) {
   res.send(""+carte);
 });
+
+config(); //process.env
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/auth', AuthentificationRoute);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server run to http://localhost:${process.env.PORT}`);
+})
